a996b9254c6e15c999ba98a5bb2c81472a027b4c
Maythee Anegboonlap <natkung@gmail.com>
2013-04-07 16:27:19 +0800

    Add resources file for web.

--- app.js
+++ app.js
@@ -5,16 +5,31 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , less_middleware = require('less-middleware')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , fs = require('fs')
+  , simplesmtp = require('simplesmtp');
 
 var app = express();
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.use(less_middleware({
+    src: __dirname + '/public',
+    force: true
+  }));
+});
+
+app.configure('production', function() {
+  app.use(less_middleware({src: __dirname + '/public'}));
+});
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'hjs');
+app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -24,14 +39,40 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var smtp = simplesmtp.createServer({
+  debug: false,
+  SMTPBanner: 'Hello, This is redirector'
+});
+smtp.listen(process.env.SMTP || 2525, function () {
+  console.log ('Redirector server listen on port ' + (process.env.SMTP || 2525));
+});
+smtp.on('startData', function (connection) {
+  console.log ('Message from:', connection.from);
+  console.log ('Message to:', connection.to);
+  connection.saveStream = fs.createWriteStream("/tmp/message.txt");
+});
+smtp.on('data', function (connection, chunk) {
+  connection.saveStream.write(chunk);
+});
+smtp.on('dataReady', function (connection, callback) {
+  connection.saveStream.end();
+  console.log("Incoming message saved to /tmp/message.txt");
+  callback(null, 'D1');
+});
+smtp.on('validateRecipient', function (connection, email, callback) {
+  var domain = 'llun.in.th';
+  var pattern = new RegExp('@' + domain + '$');
+  if (pattern.test(email)) {
+    callback(null, 'A2');
+  }
+  else {
+    callback(new Error('Not allow recipient, we redirect to other webservice only, not email'));
+  }
 });
