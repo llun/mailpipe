63e031bc0a2b9587276ff82d948705f45f4a3377
Maythee Anegboonlap <natkung@gmail.com>
2013-06-12 23:31:38 +0800

    Remove native_parser.

--- models/database.js
+++ models/database.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 var databaseURL = process.env.MONGO_URL || 'mongodb://localhost/mailpipe'
 var database = mongoose.connect(databaseURL, {
-    db: { native_parser: true },
     server: { keepAlive: 1 }
   });
 
