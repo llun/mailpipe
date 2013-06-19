df66f814876899dd0fcdd98ac74ba05fe7700588
Maythee Anegboonlap <natkung@gmail.com>
2013-06-19 23:23:25 +0800

    Added database option for MongoDB on Azure.

--- models/database.js
+++ models/database.js
@@ -1,8 +1,20 @@
 var mongoose = require('mongoose');
 
 var databaseURL = process.env.MONGO_URL || 'mongodb://localhost/mailpipe'
+
+// Below options is for Azure issue, http://stackoverflow.com/questions/13980236/does-mongodb-have-reconnect-issues-or-am-i-doing-it-wrong
 var database = mongoose.connect(databaseURL, {
-    server: { keepAlive: 1 }
+    server: { 
+      auto_reconnect: true,
+      poolSize: 10,
+      socketOptions: {
+        keepAlive: 1 
+      }
+    },
+    db: {
+      numberOfRetries: 10,
+      retryMiliSeconds: 1000
+    }
   });
 
 module.exports = database;
